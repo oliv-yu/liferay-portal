fe640335006e3a3bf25d1b1735d175f2e09259a7
Bryan Engler <bryan.engler@liferay.com>
2019-04-30 14:30:38 -0700

    portal-search-elasticsearch6-impl: add tests for nested fields

--- modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/test/java/com/liferay/portal/search/elasticsearch6/internal/document/DefaultElasticsearchDocumentFactoryTest.java
+++ modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/test/java/com/liferay/portal/search/elasticsearch6/internal/document/DefaultElasticsearchDocumentFactoryTest.java
@@ -23,7 +23,14 @@ import com.liferay.portal.search.test.util.indexing.DocumentFixture;
 
 import java.io.IOException;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.elasticsearch.common.Strings;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -46,6 +53,211 @@ public class DefaultElasticsearchDocumentFactoryTest {
 	@After
 	public void tearDown() throws Exception {
 		_documentFixture.tearDown();
+	}
+
+	@Test
+	public void testArrayOfArrays() throws IOException {
+		XContentBuilder builder = XContentFactory.jsonBuilder(
+		).startObject(
+		).field(
+			"alpha"
+		).startArray(
+		).startArray(
+		).value(
+			"one"
+		).value(
+			"two"
+		).value(
+			"three"
+		).endArray(
+		).startArray(
+		).value(
+			"four"
+		).value(
+			"five"
+		).value(
+			"six"
+		).endArray(
+		).endArray(
+		).endObject();
+
+		String expected = Strings.toString(builder);
+
+		DocumentBuilder documentBuilder = new DocumentBuilderImpl();
+
+		List<Object> valueList1 = new ArrayList<>();
+
+		valueList1.add("one");
+		valueList1.add("two");
+		valueList1.add("three");
+
+		List<Object> valueList2 = new ArrayList<>();
+
+		valueList2.add("four");
+		valueList2.add("five");
+		valueList2.add("six");
+
+		List<Object> combinedValueList = new ArrayList<>();
+
+		combinedValueList.add(valueList1);
+		combinedValueList.add(valueList2);
+
+		documentBuilder.setValues("alpha", combinedValueList);
+
+		String actual = Strings.toString(
+			_elasticsearchDocumentFactory.getElasticsearchDocument(
+				documentBuilder.build()));
+
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testArrayOfObjects() throws IOException {
+		String expected =
+			"{\"group\":\"fans\",\"user\":[{\"last\":\"Smith\",\"first\":" +
+			"\"John\"},{\"last\":\"White\",\"first\":\"Alice\"}]}";
+
+		DocumentBuilder documentBuilder = new DocumentBuilderImpl();
+
+		documentBuilder.setString("group", "fans");
+
+		List<Map<String, Object>> userFieldInnerObjectList = new ArrayList<>();
+
+		Map<String, Object> userFieldJohnSmithInnerObject = new HashMap<>();
+
+		userFieldJohnSmithInnerObject.put("first", "John");
+		userFieldJohnSmithInnerObject.put("last", "Smith");
+
+		userFieldInnerObjectList.add(userFieldJohnSmithInnerObject);
+
+		Map<String, Object> userFieldAliceWhiteInnerObject = new HashMap<>();
+
+		userFieldAliceWhiteInnerObject.put("first", "Alice");
+		userFieldAliceWhiteInnerObject.put("last", "White");
+
+		userFieldInnerObjectList.add(userFieldAliceWhiteInnerObject);
+
+		documentBuilder.setValue("user", userFieldInnerObjectList);
+
+		String actual = Strings.toString(
+			_elasticsearchDocumentFactory.getElasticsearchDocument(
+				documentBuilder.build()));
+
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testInnerObject() throws IOException {
+		XContentBuilder builder = XContentFactory.jsonBuilder(
+		).startObject(
+		).field(
+			"alpha"
+		).startObject(
+		).field(
+			"position", "1"
+		).endObject(
+		).endObject();
+
+		String expected = Strings.toString(builder);
+
+		DocumentBuilder documentBuilder = new DocumentBuilderImpl();
+
+		Map<String, Object> positionFieldInnerObject = new HashMap<>();
+
+		positionFieldInnerObject.put("position", "1");
+
+		documentBuilder.setValue("alpha", positionFieldInnerObject);
+
+		String actual = Strings.toString(
+			_elasticsearchDocumentFactory.getElasticsearchDocument(
+				documentBuilder.build()));
+
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testMultipleInnerObjects() throws IOException {
+		String expected =
+			"{\"region\":\"US\",\"manager\":{\"name\":{\"last\":\"Smith\"," +
+				"\"first\":\"John\"},\"age\":30}}";
+
+		DocumentBuilder documentBuilder = new DocumentBuilderImpl();
+
+		Map<String, Object> nameFieldInnerObject = new HashMap<>();
+
+		nameFieldInnerObject.put("first", "John");
+		nameFieldInnerObject.put("last", "Smith");
+
+		Map<String, Object> managerFieldInnerObject = new HashMap<>();
+
+		managerFieldInnerObject.put("age", 30);
+		managerFieldInnerObject.put("name", nameFieldInnerObject);
+
+		documentBuilder.setString("region", "US");
+		documentBuilder.setValue("manager", managerFieldInnerObject);
+
+		String actual = Strings.toString(
+			_elasticsearchDocumentFactory.getElasticsearchDocument(
+				documentBuilder.build()));
+
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testMultipleValuesSetAsStrings() throws IOException {
+		String expected = "{\"alpha\":[\"one\",\"two\",\"three\"]}";
+
+		DocumentBuilder documentBuilder = new DocumentBuilderImpl();
+
+		documentBuilder.setStrings("alpha", "one", "two", "three");
+
+		String actual = Strings.toString(
+			_elasticsearchDocumentFactory.getElasticsearchDocument(
+				documentBuilder.build()));
+
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testMultipleValuesSetAsValue() throws IOException {
+		String expected = "{\"alpha\":[\"one\",\"two\",\"three\"]}";
+
+		DocumentBuilder documentBuilder = new DocumentBuilderImpl();
+
+		List<Object> valueList = new ArrayList<>();
+
+		valueList.add("one");
+		valueList.add("two");
+		valueList.add("three");
+
+		documentBuilder.setValue("alpha", valueList);
+
+		String actual = Strings.toString(
+			_elasticsearchDocumentFactory.getElasticsearchDocument(
+				documentBuilder.build()));
+
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testMultipleValuesSetAsValues() throws IOException {
+		String expected = "{\"alpha\":[\"one\",\"two\",\"three\"]}";
+
+		DocumentBuilder documentBuilder = new DocumentBuilderImpl();
+
+		List<Object> valueList = new ArrayList<>();
+
+		valueList.add("one");
+		valueList.add("two");
+		valueList.add("three");
+
+		documentBuilder.setValues("alpha", valueList);
+
+		String actual = Strings.toString(
+			_elasticsearchDocumentFactory.getElasticsearchDocument(
+				documentBuilder.build()));
+
+		Assert.assertEquals(expected, actual);
 	}
 
 	@Test
