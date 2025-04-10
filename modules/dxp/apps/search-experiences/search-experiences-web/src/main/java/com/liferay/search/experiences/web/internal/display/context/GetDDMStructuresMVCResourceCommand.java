4c7b854ca54b142d2c78b19e3b091113b95bdd88
Joshua Cords <joshua.cords@liferay.com>
2025-04-10 16:28:47 -0700

    LPD-37942 search-experiences-web: add DLFileEntryTypes logic
    https://liferay.atlassian.net/browse/LPD-37942

--- modules/dxp/apps/search-experiences/search-experiences-web/src/main/java/com/liferay/search/experiences/web/internal/display/context/GetDDMStructuresMVCResourceCommand.java
+++ modules/dxp/apps/search-experiences/search-experiences-web/src/main/java/com/liferay/search/experiences/web/internal/display/context/GetDDMStructuresMVCResourceCommand.java
@@ -5,8 +5,11 @@
 
 package com.liferay.search.experiences.web.internal.display.context;
 
+import com.liferay.document.library.kernel.exception.NoSuchFileEntryTypeException;
 import com.liferay.document.library.kernel.model.DLFileEntry;
 import com.liferay.document.library.kernel.model.DLFileEntryMetadata;
+import com.liferay.document.library.kernel.model.DLFileEntryType;
+import com.liferay.document.library.kernel.service.DLFileEntryTypeLocalService;
 import com.liferay.dynamic.data.mapping.model.DDMStructure;
 import com.liferay.dynamic.data.mapping.service.DDMStructureLocalService;
 import com.liferay.petra.string.StringPool;
@@ -110,7 +113,8 @@ public class GetDDMStructuresMVCResourceCommand implements MVCResourceCommand {
 		String lookupClassName = className;
 
 		if (lookupClassName.equals(DLFileEntry.class.getName())) {
-			lookupClassName = DLFileEntryMetadata.class.getName();
+			return _getDLFileEntryTypesJSONObject(className, resourceRequest);
+			//			lookupClassName = DLFileEntryMetadata.class.getName();
 		}
 
 		List<DDMStructure> ddmStructures =
@@ -229,11 +233,104 @@ public class GetDDMStructuresMVCResourceCommand implements MVCResourceCommand {
 		return JSONUtil.put("ddmStructures", ddmStructureJSONArray);
 	}
 
+	private JSONObject _getDLFileEntryTypesJSONObject(
+		String className, ResourceRequest resourceRequest) {
+
+		List<DLFileEntryType> dlFileEntryTypes =
+			_dlFileEntryTypeLocalService.getFileEntryTypesByCompanyId(
+				_portal.getCompanyId(resourceRequest));
+
+		JSONArray dlFileEntryTypesJSONArray = _jsonFactory.createJSONArray();
+
+		int pageSize = ParamUtil.getInteger(resourceRequest, "pageSize", 10);
+		int page = ParamUtil.getInteger(resourceRequest, "page", 1);
+
+		int end = page * pageSize;
+		page = (page - 1) * pageSize;
+
+		Locale locale = LocaleUtil.fromLanguageId(
+			ParamUtil.getString(resourceRequest, "languageId"));
+
+		int size = dlFileEntryTypes.size();
+
+		if (page == 0) {
+			try {
+				DLFileEntryType dlFileEntryType =
+					_dlFileEntryTypeLocalService.
+						getBasicDocumentDLFileEntryType();
+
+				dlFileEntryTypesJSONArray.put(
+					JSONUtil.put(
+						"className", className
+					).put(
+						"ddmStructureExternalReferenceCode",
+						dlFileEntryType.getExternalReferenceCode()
+					).put(
+						"ddmStructureLocalizedName",
+						dlFileEntryType.getName(locale)
+					).put(
+						"groupExternalReferenceCode", StringPool.BLANK
+					).put(
+						"groupLocalizedName", StringPool.BLANK
+					));
+
+				end--;
+				size++;
+			}
+			catch (NoSuchFileEntryTypeException noSuchFileEntryTypeException) {
+				if (_log.isWarnEnabled()) {
+					_log.warn(noSuchFileEntryTypeException);
+				}
+			}
+		}
+
+		for (int i = page; (i < dlFileEntryTypes.size()) && (i < end); i++) {
+			try {
+				DLFileEntryType dlFileEntryType = dlFileEntryTypes.get(i);
+
+				Group group = _groupLocalService.getGroup(
+					dlFileEntryType.getGroupId());
+
+				// update naming
+
+				dlFileEntryTypesJSONArray.put(
+					JSONUtil.put(
+						"className", className
+					).put(
+						"ddmStructureExternalReferenceCode",
+						dlFileEntryType.getExternalReferenceCode()
+					).put(
+						"ddmStructureLocalizedName",
+						dlFileEntryType.getName(locale)
+					).put(
+						"groupExternalReferenceCode",
+						group.getExternalReferenceCode()
+					).put(
+						"groupLocalizedName", group.getName(locale)
+					));
+			}
+			catch (Exception exception) {
+				if (_log.isWarnEnabled()) {
+					_log.warn(exception);
+				}
+			}
+		}
+
+		return JSONUtil.put(
+			"ddmStructures", dlFileEntryTypesJSONArray
+		).put(
+			"totalCount", size
+		);
+	}
+
 	private static final Log _log = LogFactoryUtil.getLog(
 		GetDDMStructuresMVCResourceCommand.class);
 
 	@Reference
 	private DDMStructureLocalService _ddmStructureLocalService;
+
+	@Reference
+	private DLFileEntryTypeLocalService _dlFileEntryTypeLocalService;
 
 	@Reference
 	private GroupLocalService _groupLocalService;
