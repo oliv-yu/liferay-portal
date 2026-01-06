f84008ac23845eeb613e2a352eee50d00a171576
Bryan Engler <bryan.engler@liferay.com>
2026-01-06 12:56:46 -0800

    LPD-75630 portal-search-api: Add method to check if replication is initializaing
    https://liferay.atlassian.net/browse/LPD-75630

--- modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/ccr/CrossClusterReplicationConfigurationHelper.java
+++ modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/ccr/CrossClusterReplicationConfigurationHelper.java
@@ -22,4 +22,6 @@ public interface CrossClusterReplicationConfigurationHelper {
 
 	public boolean isCrossClusterReplicationEnabled();
 
+	public boolean isCrossClusterReplicationInitializing();
+
 }
