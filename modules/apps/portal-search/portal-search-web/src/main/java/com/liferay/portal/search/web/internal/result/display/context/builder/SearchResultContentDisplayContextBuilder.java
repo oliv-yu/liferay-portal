5d26db0855f185b4f591a1c4b6ca82e02d8e657a
Olivia Yu <olivia.yu@liferay.com>
2026-02-24 15:44:31 -0800

    LPD-80055 portal-search-web: Avoid NPE related to null edit URL from object folder asset renderer

--- modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/result/display/context/builder/SearchResultContentDisplayContextBuilder.java
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/result/display/context/builder/SearchResultContentDisplayContextBuilder.java
@@ -20,6 +20,7 @@ import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.search.web.internal.result.display.context.SearchResultContentDisplayContext;
 
+import jakarta.portlet.PortletURL;
 import jakarta.portlet.RenderRequest;
 import jakarta.portlet.RenderResponse;
 
@@ -69,30 +70,38 @@ public class SearchResultContentDisplayContextBuilder {
 			boolean hasEditPermission = assetRenderer.hasEditPermission(
 				_permissionChecker);
 
-			searchResultContentDisplayContext.setHasEditPermission(
-				hasEditPermission);
-
 			if (hasEditPermission) {
-				ThemeDisplay themeDisplay =
-					(ThemeDisplay)_renderRequest.getAttribute(
-						WebKeys.THEME_DISPLAY);
+				PortletURL editPortletURL = assetRenderer.getURLEdit(
+					_portal.getLiferayPortletRequest(_renderRequest),
+					_portal.getLiferayPortletResponse(_renderResponse));
 
-				searchResultContentDisplayContext.setIconEditTarget(title);
-				searchResultContentDisplayContext.setIconURLString(
-					PortletURLBuilder.create(
-						assetRenderer.getURLEdit(
-							_portal.getLiferayPortletRequest(_renderRequest),
-							_portal.getLiferayPortletResponse(_renderResponse))
-					).setRedirect(
-						themeDisplay.getURLCurrent()
-					).setPortletResource(
-						() -> {
-							PortletDisplay portletDisplay =
-								themeDisplay.getPortletDisplay();
+				if (editPortletURL != null) {
+					ThemeDisplay themeDisplay =
+						(ThemeDisplay)_renderRequest.getAttribute(
+							WebKeys.THEME_DISPLAY);
 
-							return portletDisplay.getId();
-						}
-					).buildString());
+					searchResultContentDisplayContext.setIconEditTarget(title);
+					searchResultContentDisplayContext.setIconURLString(
+						PortletURLBuilder.create(
+							editPortletURL
+						).setRedirect(
+							themeDisplay.getURLCurrent()
+						).setPortletResource(
+							() -> {
+								PortletDisplay portletDisplay =
+									themeDisplay.getPortletDisplay();
+
+								return portletDisplay.getId();
+							}
+						).buildString());
+
+					searchResultContentDisplayContext.setHasEditPermission(
+						true);
+				}
+				else {
+					searchResultContentDisplayContext.setHasEditPermission(
+						false);
+				}
 			}
 
 			searchResultContentDisplayContext.setShowExtraInfo(
