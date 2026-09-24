97077122c16ab88b832d639bae43e9018bfac6e8
Olivia Yu <olivia.yu@liferay.com>
2026-09-24 11:54:25 -0700

    LPD-105320 portal-search-opensearch2-impl: Rename the test methods
    Each test now leads with getHighlightQuery, the one public method the class exposes, so the name says what is under test before it says what the case is.
    
    https://liferay.atlassian.net/browse/LPD-105320

--- modules/apps/portal-search-opensearch2/portal-search-opensearch2-impl/src/test/java/com/liferay/portal/search/opensearch2/internal/highlight/HighlightQueryUtilTest.java
+++ modules/apps/portal-search-opensearch2/portal-search-opensearch2-impl/src/test/java/com/liferay/portal/search/opensearch2/internal/highlight/HighlightQueryUtilTest.java
@@ -33,14 +33,14 @@ public class HighlightQueryUtilTest {
 		LiferayUnitTestRule.INSTANCE;
 
 	@Test
-	public void testKeepsExactPhraseQuery() {
+	public void testGetHighlightQueryKeepsExactPhraseQuery() {
 		Query query = _createMatchPhraseQuery(null);
 
 		Assert.assertSame(query, HighlightQueryUtil.getHighlightQuery(query));
 	}
 
 	@Test
-	public void testKeepsFilterQueryClauses() {
+	public void testGetHighlightQueryKeepsFilterQueryClauses() {
 		BoolQuery.Builder boolQueryBuilder = QueryBuilders.bool();
 
 		Query filterQuery = _createMatchQuery();
@@ -63,7 +63,7 @@ public class HighlightQueryUtilTest {
 	}
 
 	@Test
-	public void testKeepsMustNotQueryClauses() {
+	public void testGetHighlightQueryKeepsMustNotQueryClauses() {
 		BoolQuery.Builder boolQueryBuilder = QueryBuilders.bool();
 
 		Query query = _createMatchQuery();
@@ -79,7 +79,7 @@ public class HighlightQueryUtilTest {
 	}
 
 	@Test
-	public void testKeepsProximityQueryInMustQueryClauses() {
+	public void testGetHighlightQueryKeepsProximityQueryInMustQueryClauses() {
 		BoolQuery.Builder boolQueryBuilder = QueryBuilders.bool();
 
 		Query proximityQuery = _createMatchPhraseQuery(50);
@@ -94,21 +94,21 @@ public class HighlightQueryUtilTest {
 	}
 
 	@Test
-	public void testKeepsQueryWithoutProximityQuery() {
+	public void testGetHighlightQueryKeepsQueryWithoutProximityQuery() {
 		Query query = _createMatchQuery();
 
 		Assert.assertSame(query, HighlightQueryUtil.getHighlightQuery(query));
 	}
 
 	@Test
-	public void testKeepsStandaloneProximityQuery() {
+	public void testGetHighlightQueryKeepsStandaloneProximityQuery() {
 		Query query = _createMatchPhraseQuery(50);
 
 		Assert.assertSame(query, HighlightQueryUtil.getHighlightQuery(query));
 	}
 
 	@Test
-	public void testRemovesProximityQueryFromBooleanQuery() {
+	public void testGetHighlightQueryRemovesProximityQueryFromBooleanQuery() {
 		BoolQuery.Builder boolQueryBuilder = QueryBuilders.bool();
 
 		Query exactPhraseQuery = _createMatchPhraseQuery(null);
@@ -128,7 +128,7 @@ public class HighlightQueryUtilTest {
 	}
 
 	@Test
-	public void testRemovesProximityQueryFromInnerBooleanQuery() {
+	public void testGetHighlightQueryRemovesProximityQueryFromInnerBooleanQuery() {
 		BoolQuery.Builder boolQueryBuilder = QueryBuilders.bool();
 		BoolQuery.Builder innerBoolQueryBuilder = QueryBuilders.bool();
 
@@ -157,7 +157,7 @@ public class HighlightQueryUtilTest {
 	}
 
 	@Test
-	public void testRemovesProximityQueryFromShouldQueryClausesUnderMust() {
+	public void testGetHighlightQueryRemovesProximityQueryFromShouldUnderMust() {
 		BoolQuery.Builder boolQueryBuilder = QueryBuilders.bool();
 		BoolQuery.Builder innerBoolQueryBuilder = QueryBuilders.bool();
 		BoolQuery.Builder middleBoolQueryBuilder = QueryBuilders.bool();
@@ -193,7 +193,7 @@ public class HighlightQueryUtilTest {
 	}
 
 	@Test
-	public void testRemovesProximityQueryLeavingNothing() {
+	public void testGetHighlightQueryRemovesProximityQueryLeavingNothing() {
 		BoolQuery.Builder boolQueryBuilder = QueryBuilders.bool();
 
 		boolQueryBuilder.should(_createMatchPhraseQuery(50));
@@ -204,12 +204,12 @@ public class HighlightQueryUtilTest {
 	}
 
 	@Test
-	public void testReturnsNullForNullQuery() {
+	public void testGetHighlightQueryReturnsNullForNullQuery() {
 		Assert.assertNull(HighlightQueryUtil.getHighlightQuery(null));
 	}
 
 	@Test
-	public void testReturnsNullWhenOnlyFilterQueryClausesSurvive() {
+	public void testGetHighlightQueryReturnsNullWhenOnlyFiltersSurvive() {
 		BoolQuery.Builder boolQueryBuilder = QueryBuilders.bool();
 
 		boolQueryBuilder.filter(_createMatchQuery());
